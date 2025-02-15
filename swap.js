c79e0f222dec6e0402175175d45dcaa839e353ab
shafayatGit <shafayathossain.drmc@gmail.com>
2025-02-15 11:17:46 +0600

    done

---
+++ swap.js
@@ -0,0 +1,18 @@
+//Will swap the value of variable
+let a = 6;
+let b = 3;
+
+let temp = a; //swapping er jonno ekta temporary variable dhore nite hobe.
+a=b;
+b=temp;
+console.log('a:',a,'b:',b)
+
+
+
+//EASIEST WAY ----> Destructuring Method   
+let p =50;
+let q ='shafayat';
+
+[p,q] = [q,p]
+console.log('p:',p,'q:',q)
+
